33dbd2f79be5f76e3977bc927b3653a9417ca936
giangdhpham <giangdhpham@kms-technology.com>
2022-05-05 22:11:11 +0700

    Add project files.

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,157 @@
+﻿using static System.Console;
+
+var apple = new Product("Apple", Color.Green, Size.Small);
+var tree = new Product("Tree", Color.Green, Size.Large);
+var house = new Product("House", Color.Blue, Size.Large);
+
+Product[] products = { apple, tree, house };
+var pf = new ProductFilter();
+WriteLine("Green produts (old):");
+foreach(var p in pf.FilterByColor(products, Color.Green))
+{
+    WriteLine($" - {p.Name} is green");
+}
+
+var bf = new BetterFilter();
+WriteLine("Green products (new):");
+foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+{
+    WriteLine($" - {p.Name} is green");
+}
+
+WriteLine("Large blue items");
+foreach (var p in bf.Filter(products, 
+    new AndSpecification<Product>(
+        new ColorSpecification(Color.Blue), 
+        new SizeSpecification(Size.Large) 
+        )
+    ))
+{
+    WriteLine($" - {p.Name} is large and blue");
+}
+
+public enum Color
+{
+    Red, Green, Blue
+}
+
+public enum Size
+{
+    Small, Medium, Large, Yuge
+}
+
+public class Product
+{
+    public string Name;
+    public Color Color;
+    public Size Size;
+
+    public Product(string name, Color color, Size size)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Color = color;
+        Size = size;
+    }
+}
+
+#region Don't
+public class ProductFilter
+{
+    public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+    {
+        foreach (Product product in products)
+        {
+            if (product.Size == size)
+                yield return product;
+        }
+    }
+
+    public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+    {
+        foreach (Product product in products)
+        {
+            if (product.Color == color)
+                yield return product;
+        }
+    }
+
+    public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+    {
+        foreach (Product product in products)
+        {
+            if (product.Size == size && product.Color == color)
+                yield return product;
+        }
+    }
+}
+#endregion
+
+#region Do
+public interface ISpecification<T>
+{
+    bool IsSatisfied(T t);
+}
+
+public interface IFilter<T>
+{
+    IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+}
+
+public class ColorSpecification : ISpecification<Product>
+{
+    private Color color;
+
+    public ColorSpecification(Color color)
+    {
+        this.color = color;
+    }
+
+    public bool IsSatisfied(Product t)
+    {
+        return t.Color == color;
+    }
+}
+
+public class SizeSpecification : ISpecification<Product>
+{
+    private Size size;
+
+    public SizeSpecification(Size size)
+    {
+        this.size = size;
+    }
+
+    public bool IsSatisfied(Product t)
+    {
+        return t.Size == size;
+    }
+}
+
+public class AndSpecification<T> : ISpecification<T>
+{
+    private ISpecification<T> first, second;
+
+    public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+    {
+        this.first = first ?? throw new ArgumentNullException(nameof(first));
+        this.second = second ?? throw new ArgumentNullException(nameof(second));
+    }
+
+    public bool IsSatisfied(T t)
+    {
+        return first.IsSatisfied(t) && second.IsSatisfied(t);
+    }
+}
+
+public class BetterFilter : IFilter<Product>
+{
+    public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+    {
+        foreach (var item in items)
+        {
+            if (spec.IsSatisfied(item))
+                yield return item;
+        }
+    }
+}
+#endregion
---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,59 @@
+﻿using static System.Console;
+
+var j = new Journal();
+j.AddEntry("I cried today");
+j.AddEntry("I ate a bug");
+WriteLine(j);
+
+var p = new Persistence();
+var filename = @".\..\..\..\journal.txt";
+p.SaveToFile(j, filename, true);
+
+public class Journal
+{
+    private readonly List<string> entires = new List<string>();
+    private static int count = 0;
+
+    public int AddEntry(string text)
+    {
+        entires.Add($"{++count}: {text}");
+        return count;
+    }
+
+    public void RemoveEntry(int index)
+    {
+        entires.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, entires);
+    }
+
+    #region Don't
+    //Add to much responsibilities to the Journal class
+    public void Save(string filename)
+    {
+        File.WriteAllText(filename, ToString());
+    }
+
+    public static Journal Load(string filename)
+    {
+        return null;
+    }
+
+    public void Load(Uri uri) { }
+    #endregion
+}
+
+#region Do
+public class Persistence
+{
+    public void SaveToFile(Journal j, string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+            File.WriteAllText(filename, j.ToString());
+    }
+}
+#endregion
+
