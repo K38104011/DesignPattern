3a626164754260ecb380ab15314475deacaa0e67
giangdhpham <giangdhpham@kms-technology.com>
2022-05-07 17:54:14 +0700

    Builder Pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+var builder = new HtmlBuilder("ul");
+builder.AddChild("li", "Hello");
+builder.AddChild("li", "World");
+Console.WriteLine(builder);
+
+public class HtmlBuilder
+{
+    private readonly string rootName;
+    HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(string rootName)
+    {
+        this.rootName = rootName;
+        root.Name = rootName;
+    }
+
+    public void AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+    }
+
+    public override string ToString()
+    {
+        return root.ToString();
+    }
+
+    public void Clear()
+    {
+        root = new HtmlElement { Name = rootName };
+    }
+}
+
+public class HtmlElement
+{
+    public string Name, Text;
+    public List<HtmlElement> Elements = new List<HtmlElement>();
+    private const int indentSize = 2;
+
+    public HtmlElement() { }
+
+    public HtmlElement(string name, string text)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Text = text ?? throw new ArgumentNullException(nameof(text));
+    }
+
+    private string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', indent * indentSize);
+        sb.AppendLine($"{i}<{Name}>");
+
+        if (!string.IsNullOrEmpty(Text))
+        {
+            sb.Append(new string(' ', (indent + 1) * indentSize));
+            sb.AppendLine(Text);
+        }
+
+        foreach (var e in Elements)
+        {
+            sb.Append(e.ToStringImpl(indent + 1));
+        }
+
+        sb.AppendLine($"{i}</{Name}>");
+        return sb.ToString();
+    }
+
+    public override string ToString()
+    {
+        return ToStringImpl(0);
+    }
+}
