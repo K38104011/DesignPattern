c033007bd39db2df0f37e503b94cabc0a88933ee
giangdhpham <giangdhpham@kms-technology.com>
2022-05-09 21:54:00 +0700

    Observeer

---
+++ Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿var person = new Person();
+person.FallsIll += CallDoctor;
+person.CatchACold();
+person.FallsIll -= CallDoctor;
+person.CatchACold();
+
+void CallDoctor(object? sender, FallsIllEventArgs e)
+{
+    Console.WriteLine($"A doctor has been called to {e.Address}");
+}
+
+public class FallsIllEventArgs
+{
+    public string Address;
+}
+
+public class Person
+{
+    public event EventHandler<FallsIllEventArgs> FallsIll;
+
+    public void CatchACold()
+    {
+        FallsIll?.Invoke(this, new FallsIllEventArgs { Address = "177 Bui Huu Nghia" });
+    }
+}
