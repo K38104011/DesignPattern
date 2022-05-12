c7db053e613e5b66923a5ee209ef6b9b62de8698
giangdhpham <giangdhpham@kms-technology.com>
2022-05-12 23:13:03 +0700

    Factory Method

---
+++ FactoriesPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FactoriesPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new PersonFactory();
+            Console.WriteLine(factory.CreatePerson("first").Id);
+            Console.WriteLine(factory.CreatePerson("second").Id);
+        }
+    }
+
+    public class PersonFactory
+    {
+        private int _index = 0;
+
+        public Person CreatePerson(string personName)
+        {
+            return new Person
+            {
+                Id = _index++,
+                Name = personName,
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿var point1 = Point.Factory.NewCartesianPoint(1, 1);
+             //Task.Factory.StartNew()
+
+Console.WriteLine(point1);
+var point2 = Point.Factory.NewPolarPoint(1, 1);
+Console.WriteLine(point2);
+
+Console.WriteLine(Point.WrongOrigin == Point.WrongOrigin);      //always create new object
+Console.WriteLine(Point.CorrectOrigin == Point.CorrectOrigin);  //initialize once
+
+
+public class Point
+{
+    #region Factory Method
+    //public static Point NewCartesianPoint(double x, double y)
+    //{
+    //    return new Point(x, y);
+    //}
+
+    //public static Point NewPolarPoint(double rho, double theta)
+    //{
+    //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    //}
+    #endregion
+
+    private double x, y;
+    private Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return $"{x}, {y}";
+    }
+
+    public static Point WrongOrigin => new Point(0, 0);
+    public static Point CorrectOrigin = new Point(0, 0);
+
+
+    //Separation of concern
+    #region Factory
+    public static class Factory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+    #endregion
+}
