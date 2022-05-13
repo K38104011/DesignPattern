a9406431577de524e49d168985c0b700d438b705
giangdhpham <giangdhpham@kms-technology.com>
2022-05-13 14:49:52 +0700

    Composition Practice

---
+++ CompositionPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+var singleValue = new SingleValue { Value = 1 };
+var manyValue = new ManyValues();
+manyValue.Add(1);
+manyValue.Add(1);
+manyValue.AddRange(singleValue);
+Console.WriteLine(manyValue.Sum());
+
+
+public interface IValueContainer : IEnumerable<int>
+{
+
+}
+
+public class SingleValue : IValueContainer
+{
+    public int Value;
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        yield return Value;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class ManyValues : List<int>, IValueContainer
+{
+
+}
+
+public static class ExtensionMethods
+{
+    public static int Sum(this List<IValueContainer> containers)
+    {
+        int result = 0;
+        foreach (var c in containers)
+            foreach (var i in c)
+                result += i;
+        return result;
+    }
+}
