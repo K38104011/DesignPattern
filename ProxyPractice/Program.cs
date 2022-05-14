c22d58a437aa7d660a578e848dd75e32357b7a7b
giangdhpham <giangdhpham@kms-technology.com>
2022-05-14 20:19:36 +0700

    Proxy Practice

---
+++ ProxyPractice/Program.cs
@@ -0,0 +1,59 @@
+﻿var person = new Person();
+person.Age = 17;
+var resPerson = new ResponsiblePerson(person);
+Console.WriteLine(resPerson.Drink());
+Console.WriteLine(resPerson.Drive());
+Console.WriteLine(resPerson.DrinkAndDrive());
+
+public class Person
+{
+    public int Age { get; set; }
+
+    public string Drink()
+    {
+        return "drinking";
+    }
+
+    public string Drive()
+    {
+        return "driving";
+    }
+
+    public string DrinkAndDrive()
+    {
+        return "driving while drunk";
+    }
+}
+
+public class ResponsiblePerson
+{
+    private Person _person;
+
+    public ResponsiblePerson(Person person)
+    {
+        _person = person;
+    }
+
+    public string Drink()
+    {
+        if (_person.Age > 18) return _person.Drink();
+        return "too young";
+    }
+
+    public string Drive()
+    {
+        if (_person.Age > 16) return _person.Drive();
+        return "too young";
+    }
+
+    public string DrinkAndDrive()
+    {
+        return "dead";
+    }
+
+    public int Age 
+    { 
+        get { return _person.Age;  } 
+        set { _person.Age = value; }
+    }
+}
